b2fe273305c029024b4b331724bef26990a61231
fragaeverton <evertonfraga90@hotmail.com>
2024-03-25 16:27:40 +0000

    ADD bot replies

---
+++ README.md
@@ -0,0 +1,51 @@
+# Bot
+
+Bot is a chatbot application that utilizes the Venon Library, a Whatsapp API, to provide seamless communication through WhatsApp.
+
+## Introduction
+
+Bot is designed to streamline communication processes by integrating with WhatsApp through the Venon Library. It offers a convenient solution for businesses, organizations, or individuals who want to automate responses, handle inquiries, or interact with users efficiently through the popular messaging platform.
+
+## Features
+
+- **WhatsApp Integration:** Utilizes the Venon Library to integrate seamlessly with WhatsApp.
+- **Chatbot Capabilities:** Offers basic chatbot functionalities to automate responses.
+- **Customization:** Allows customization of responses, commands, and interactions.
+- **Scalability:** Can be scaled to accommodate large volumes of users and messages.
+- **Ease of Use:** User-friendly interface for easy setup and management.
+
+## Getting Started
+
+To get started with Bot, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Install the necessary dependencies.
+3. Configure your WhatsApp account credentials in the configuration file.
+4. Run the application.
+
+```bash
+git clone https://github.com/fragaeverton/Bot.git
+cd Bot
+npm install                                                                  
+```
+
+## Usage
+
+Once the application is set up and running, users can interact with the chatbot through WhatsApp. Customize the responses and commands according to your requirements to tailor the experience for your users.
+
+## Dependencies
+
+- [Venon Library](https://github.com/orkestral/venom): WhatsApp API library used for integration.
+
+## Contributing
+
+Contributions are welcome! If you have any ideas, suggestions, or improvements, feel free to open an issue or create a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+We would like to express our gratitude to the creators and maintainers of the Venon Library for providing a powerful tool for WhatsApp integration.
+
--- index.js
+++ index.js
@@ -1,27 +1,109 @@
 // Supports ES6
 // import { create, Whatsapp } from 'venom-bot';
 const venom = require('venom-bot');
+const GROUP_ID = require('./prop.js');
+let products = [];
+let rgex_clear = /CLEAR/;
+let rgex_view = /VIEW/;
+let rgex_delete = /DELETE/;
 
-venom
-  .create({
-    session: 'session-name' //name of session
-  })
-  .then((client) => start(client))
-  .catch((erro) => {
-    console.log(erro);
-  });
+venom.create({
+  session: 'session-name' //name of session
+})
+.then((client) => {
+  start(client);
+})
+.catch((erro) => {
+  console.log(erro);
+});
+
 
 function start(client) {
-  client.onMessage((message) => {
-    if (message.body === 'Hi' && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, 'Welcome Venom 🕷')
-        .then((result) => {
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro); //return object error
-        });
+  
+  client.onStateChange((state) => {
+    console.log('State changed: ', state);
+    // force whatsapp take over
+    if ('CONFLICT'.includes(state)) client.useHere();
+    // detect disconnect on whatsapp
+    if ('UNPAIRED'.includes(state)) console.log('logout');
+  });
+
+  let time = 0;
+  client.onStreamChange((state) => {
+    console.log('State Connection Stream: ' + state);
+    clearTimeout(time);
+    if (state === 'DISCONNECTED' || state === 'SYNCING') {
+      time = setTimeout(() => {
+        client.close();
+      }, 80000);
     }
   });
+
+  // function to detect incoming call
+  client.onIncomingCall(async (call) => {
+    console.log(call);
+    client.sendText(call.peerJid, "Sorry, I still can't answer calls");
+  });
+  
+  client
+  .getAllMessagesInChat(GROUP_ID)
+  .then((result) => {
+    let msg = result[result.length - 1].body;
+    if(msg!="*Your list is empty*"){
+      products = msg.split("\n").map(item => item.toUpperCase());
+    }
+  })
+  .catch((erro) => {
+  });
+
+  client.onMessage((message) => {
+    if (/*message.body === '.'*/ message.from == GROUP_ID && message.isGroupMsg === false) { 
+      let response = analyze(message);
+      //let reply = generateReply();
+      sendMessage(client, message, response);
+      
+    }
+  });
+}
+
+function analyze({body}){
+  switch(true){
+    case rgex_clear.test(body):
+      products = [];
+      break;
+    case rgex_view.test(body):
+      break;
+    case rgex_delete.test(body):
+      let target = body.replace("DELETE ", "").toUpperCase();
+      products = products.filter(e => e !== target);
+      break;
+    default:
+      let arr = body.split(/\n|,/).map(item => item.trim().toUpperCase());
+      products = products.concat(arr);
+  }
+  return printFormatted();
+}
+
+function printFormatted(){
+  let reply = "";
+  products.forEach(e=> {if(e != "")reply += e + "\n"});
+  if(products.length ==0){
+    reply = "*Your list is empty*";
+  }
+  return reply;
+}
+
+function generateReply(){
+
+}
+
+function sendMessage(client, msg, reply){
+  client
+    .sendText(msg.from, reply)
+    .then((result) => {
+      //console.log('Result: ', result); //return object success
+    })
+    .catch((erro) => {
+      console.error('Error when sending: ', erro); //return object error
+    });
 }
